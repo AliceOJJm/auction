8d1065eea8e16b19f6b3c80764a9f9df1f8d2178
AliceOJJm <AliceOJJm@gmail.com>
2016-05-07 14:49:05 +0300

    Fixed close lot worker

--- server/app/workers/lot_updater.rb
+++ server/app/workers/lot_updater.rb
@@ -5,7 +5,7 @@ class LotUpdater
     lots = Lot.includes(:bids).where('expires_at < ?', DateTime.now.in_time_zone('UTC'))
     lots.each do |lot|
       last_bid = lot.bids.last
-      last_bid.update!(is_final: true) unless last_bid.is_final
+      last_bid.update!(is_final: true) unless !last_bid && last_bid.is_final
     end
   end
 end
